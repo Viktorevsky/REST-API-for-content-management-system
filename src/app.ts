125701671991aebbcf6e8e2e924319bd4b19bd23
Viktor <vikojanarakelyan@gmail.com>
2026-05-10 20:38:15 +0200

    adding JWT auth

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import Fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import posts from './routes/posts'
 import users from './routes/users'
+import auth from './routes/auth'
 
 const app = Fastify({logger: true})
 
+app.register(jwt, {
+  secret: process.env.JWT_SECRET!
+})
+
 app.get('/', async (request, reply) => {
-    
   return { status: 'ok' }
 })
 
 app.register(users)
 app.register(posts)
-
+app.register(auth)
 export default app
---
+++ src/routes/auth.ts
@@ -0,0 +1,61 @@
+import { FastifyInstance } from "fastify";
+import prisma from "../lib/prisma";
+import bcrypt from "bcrypt";
+
+export default async function auth(app: FastifyInstance) {
+  app.post("/auth/login", async (request, reply) => {
+    const { username,email, password } = request.body as {
+      username?: string;
+      email?: string;
+      password?: string;
+    };
+
+    if (!email || !password || !username) {
+      return reply.status(400).send({ error: "Email, username, and password are required" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return reply.status(401).send({ error: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return reply.status(401).send({ error: "Invalid credentials" });
+    }
+
+    const token = await app.jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      { expiresIn: "7d" }
+    );
+
+    return reply.send({ accessToken: token });
+  })
+  app.post("/auth/register", async (request, reply) => {
+    const { username, email, password } = request.body as {
+      username?: string;
+      email?: string;
+      password?: string;
+    }
+
+    if (!username || !email || !password) {
+      return reply.status(400).send({ error: "Username, email, and password are required" });
+    }
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+        return reply.status(409).send({ error: "User with this email already exists" });
+    }
+    
+    const HashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: { username, email, password: HashedPassword },
+    });
+
+    const token = await app.jwt.sign(
+      { id: newUser.id, email: newUser.email, role: newUser.role },
+      { expiresIn: "7d" }
+    );
+
+    return reply.send({ accessToken: token });
+})
