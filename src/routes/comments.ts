ce39a9a756626729724d9b8807d4e6e469738ec0
Viktor <vikojanarakelyan@gmail.com>
2026-05-22 19:12:03 +0200

    fixing routes bugs

---
+++ src/routes/comments.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from 'fastify'
+import prisma from '../lib/prisma'
+import authenticate from '../hooks/authenticate'
+
+export default async function(app: FastifyInstance) {
+  app.get('/comments', async (request, reply) => {
+    return await prisma.comment.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    })
+  })
+
+  app.post('/comments',{preHandler: authenticate}, async (request, reply) => {
+
+    const { text, postId} = request.body as { text:string,postId : number,}
+    const userId = request.user.id
+    const newComment = await prisma.comment.create({
+      data: { text, postId,userId},
+    })
+    return reply.status(201).send(newComment)
+  })
+
+  app.delete('/comments/:id', { preHandler: authenticate }, async (request, reply) => {
+    const currentUser = request.user
+    const { id } = request.params as { id: string }
+
+    const comment = await prisma.comment.findUnique({ 
+        where: { id: Number(id) } 
+    })
+  if (!comment) {
+    return reply.status(404).send({ error: 'Comment not found' })
+  }
+
+  if (comment.userId !== currentUser.id && currentUser.role !== 'admin') {
+  return reply.status(403).send({ error: 'Forbidden' })
+    }
+
+  await prisma.comment.delete({ where: { id: Number(id) } })
+  return reply.status(204).send()
+})
+}
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,18 +1,22 @@
 import { FastifyInstance } from 'fastify';
 import prisma from '../lib/prisma'
+import { createPostSchema } from '../schemas/posts.schema'
+import authenticate from '../hooks/authenticate'
 
 export default async function(app: FastifyInstance){
 app.get('/posts', async (request, reply) => {
-  return await prisma.post.findMany()
+  return await prisma.post.findMany({
+    include: {
+      author:   { select: { id: true, username: true } },
+      category: { select: { id: true, name: true } }
+    }
+  })
 })
 
-app.post('/posts', async (request, reply) => {
-  let { title, content,authorId ,categoryId } = request.body as { 
-    title?: string; content?: string; authorId?: number; categoryId?: number }
-  if(!title || !content || !authorId || !categoryId) {
-    return reply.status(400).send({ error: 'All fields are required' })  
-  }
-  let newPost = await prisma.post.create({data: { title, content, authorId ,categoryId}})
+app.post('/posts', {preHandler: authenticate}, async (request, reply) => {
+  const authorId = request.user.id  
+  const { title, content, categoryId } = createPostSchema.parse(request.body)
+  const newPost = await prisma.post.create({data: { title, content, authorId ,categoryId}})
   return reply.status(201).send(newPost)
 })
 }
